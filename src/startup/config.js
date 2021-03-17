e49fe3415ebe341c8fe266249473b12394b58c27
turakijnr <btnuuman@gmail.com>
2021-03-17 10:04:14 +0100

    fixes

--- src/startup/config.js
+++ src/startup/config.js
@@ -1,8 +1,10 @@
-const config = require("config");
-const keys = require("../../config/keys");
 
-module.exports = function () {
-  if (!keys.jwtPrivateKey) {
-    throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
-  }
-};
+const config = require('config');
+
+module.exports = function (){
+
+    if (!config.get('jwtPrivateKey')){
+        throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+       
+    }
+}
