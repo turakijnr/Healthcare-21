34e7a0882d4f59029536273479f7c167bc76f0f1
turakijnr <btnuuman@gmail.com>
2021-03-17 12:00:02 +0100

    db connected

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const config = require("config");
-require("./src/db/mongoose");
+require("./src/db/mongoose")();
 require("./src/startup/config")();
 const users = require("./src/routes/users");
 const auth = require("./src/routes/auth");
@@ -8,23 +8,23 @@ const drugs = require("./src/routes/drugs");
 const sales = require("./src/routes/sales");
 const dashboard = require("./src/routes/dashboard");
 const cors = require("cors");
-const helmet = require('helmet')
-const compression = require('compression')
-const port = process.env.PORT 
+const helmet = require("helmet");
+const compression = require("compression");
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet())
-app.use(compression())
+app.use(helmet());
+app.use(compression());
 app.use(users);
 app.use(auth);
 app.use(drugs);
 app.use(sales);
 app.use(dashboard);
 
-  const server =app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log("server is up on port" + port);
 });
 
-module.exports = server
+module.exports = server;
--- src/models/user.js
+++ src/models/user.js
@@ -1,51 +1,47 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {
-      type:String,
-      required: true,
-      minlength: 5,
-      maxlength: 50
-
-    },
-    email: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 255,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 1025
-    },
-    isAdmin: Boolean
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1025,
+  },
+  isAdmin: Boolean,
 });
 
-userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({_id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
-    return token;
-}
-
-
-const User =  mongoose.model('User', userSchema);
-
-function validateUser(user) {
-    const schema = Joi.object({
-      name: Joi.string().min(5).max(50).required(),
-      email: Joi.string().min(5).max(255).required().email(),
-      password: Joi.string().min(5).max(255).required()
-  
-  
-    });
-    return schema.validate(user);
-  
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
 };
 
-module.exports.User = User;
-module.exports.validateUser = validateUser; 
+function validateUser(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+  return schema.validate(user);
+}
+
+module.exports.User = mongoose.model("User", userSchema);
+module.exports.validateUser = validateUser;
